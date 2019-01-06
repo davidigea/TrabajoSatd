f48a969f533e522e76c7b8016d19693c6240ad35
Alberto <697589@unizar.es>
2019-01-06 12:22:58 +0100

    Created some agent and behaviour classes
    Agente archivo, resultado, lector CSV (comportamiento)

--- src/comportamiento/LectorCSV_behaviour.java
+++ src/comportamiento/LectorCSV_behaviour.java
@@ -1,11 +1,43 @@
 package comportamiento;
 
 import jade.core.behaviours.Behaviour;
+import au.com.bytecode.opencsv.CSVReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class LectorCSV_behaviour  extends Behaviour {
+    String path;
+    CSVReader reader;
+
+    public LectorCSV_behaviour(String path){
+        this.path = path;
+    }
+
     @Override
     public void action() {
+        String[] fila;
+        ArrayList<String[]> resultado = new ArrayList<>();
 
+        try {
+            reader = new CSVReader(new FileReader(path));
+
+            while ((fila = reader.readNext()) != null){
+                resultado.add(fila);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        while (true) {
+            //Esperar a una petición de fichero
+            //Envía un array con el contenido del fichero
+        }
     }
 
     @Override
